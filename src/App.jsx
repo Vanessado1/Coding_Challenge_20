54f1e4c57d7a25db41aca93c52742c6c8e338cea
Vanessa Do <xex.vanessa@gmail.com>
2025-04-20 12:45:59 -0400

    created styles and imported it to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Gallery from "./components/Gallery";
 import DestinationSelector from "./components/DestinationSelector";
-import styles from "./styles/styles"
+import './styles/styles.css';
+
 
 const App = () => {
   const [tours, setTours] = useState([]);
